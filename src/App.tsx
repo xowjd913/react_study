7021c3410311d397ff2eff3eb44ed16cf5394d37
TJKim <xowjd913@gmail.com>
2026-01-15 21:51:28 +0900

    메인 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Layout } from "./layouts/Layout";
+import { Example } from "./pages/Example";
 
 function App() {
+  return (
+    <Layout>
+      <Example></Example>
+    </Layout>
+  );
 }
 
 export default App;
